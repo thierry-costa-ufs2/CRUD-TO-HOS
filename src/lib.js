72d6d07344e540bd9bf185ca97bc273934133f73
Thierry Costa <thierry.costa@dcomp.ufs.br>
2025-09-01 20:59:43 -0300

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -529,7 +529,7 @@ export const Music = {
   renderFilterOptions, sortSongs, getUnique,
   getSongWithCover: async (song) => {
     try {
-      const response = await fetch('http://localhost:3000/api/get-song-preview', {
+      const response = await fetch('/api/get-song-preview', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -558,4 +558,5 @@ export const Music = {
       return { ...song, capa: './img/default-cover.png' };
     }
   }
+
 };
