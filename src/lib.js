af45d721eec1a79ecc46868f71c103f8ecb95ef8
Thierry Costa <thierry.costa@dcomp.ufs.br>
2025-09-01 21:12:54 -0300

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -529,7 +529,7 @@ export const Music = {
   renderFilterOptions, sortSongs, getUnique,
   getSongWithCover: async (song) => {
     try {
-      const response = await fetch('resomusic.netlify.app/api/get-song-preview', {
+      const response = await fetch('/.netlify/functions/get-song-preview', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -560,4 +560,5 @@ export const Music = {
   }
 
 };
+
 
